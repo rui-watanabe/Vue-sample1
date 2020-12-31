2aa6deacc2fde7436c462b8065ade488eb21037b
rui-watanabe <wataru610806@gmail.com>
2020-12-31 22:03:37 +0900

    add basic fetchdata

--- pages/home.vue
+++ pages/home.vue
@@ -6,15 +6,39 @@
      ログインに成功！
    </p>
    {{ indicateUser }}
+   <button
+     type="button"
+     @click="fetchData"
+   >
+     fetchData
+   </button>
+   <div id="patient" />
  </div>
 </template>
 
 <script>
+import firebase from '@/plugins/firebase'
+
 export default {
+  data () {
+   return {
+     patient: {
+       name: ""
+     },
+   }
+ },
   computed: {
     indicateUser() {
       return this.$store.getters.getUser
     }
+  },
+  methods: {
+    fetchData () {
+     let Ref = firebase.database().ref('/')
+     Ref.on('value', function(snapshot){
+       document.getElementById("patient").innerHTML = snapshot.child("name").val()
+     })
+   },
   }
 }
 </script>
